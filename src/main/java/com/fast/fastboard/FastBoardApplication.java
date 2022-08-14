17ad5939f99ed97984bc3ccc86fba6414a8eb027
Hoon <yanghoon5@gmail.com>
2022-08-14 16:48:58 +0900

    #1 - Create Spring Boot Project
    - Dependencies : Web, Lombok, DevTools, Actuator

---
+++ src/main/java/com/fast/fastboard/FastBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fast.fastboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastBoardApplication.class, args);
+	}
+
+}
