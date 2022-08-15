cb062da9eb41ecb3c8133737dff7cddc63726ea9
Hoon <yanghoon5@gmail.com>
2022-08-15 19:39:10 +0900

    #12 Configure Data REST
    * Add @RepositoryRestResource to JpaRepository
    * Configure base-path and detection-strategy

--- src/main/java/com/fast/fastboard/repo/ArticleRepository.java
+++ src/main/java/com/fast/fastboard/repo/ArticleRepository.java
@@ -1,11 +1,11 @@
 package com.fast.fastboard.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.fast.fastboard.domain.Article;
 
-@Repository
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     
 }
--- src/main/java/com/fast/fastboard/repo/CommentRepository.java
+++ src/main/java/com/fast/fastboard/repo/CommentRepository.java
@@ -1,11 +1,11 @@
 package com.fast.fastboard.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.fast.fastboard.domain.Comment;
 
-@Repository
+@RepositoryRestResource
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     
 }
